49c9f6c7ca0792219049ef396ad126d72448a059
SaiKishoret99 <saikishoret99@gmail.com>
2018-07-14 15:20:45 +0530

    basic setup done

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,55 @@
 const path = require('path');
-
+const srcPath = './src/boilerplate/';
+const phaser = path.join(__dirname, 'node_modules/phaser/dist/phaser.min.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const outDir = 'www'
 module.exports = {
     mode: "development",
     devtool: "inline-source-map",
-    entry: "./src/app.ts",
+    entry: srcPath + 'game.ts',
     output: {
-        path: path.resolve(__dirname, 'www/js'),
-        filename: "app.js"
+        path: path.resolve(__dirname, outDir, 'build'),
+        filename: "main.js"
     },
     resolve: {
-      // Add `.ts` and `.tsx` as a resolvable extension.
-      extensions: [".ts", ".tsx", ".js"]
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: [".ts", ".tsx", ".js"],
+        alias: {
+            phaser: phaser
+        }
     },
     module: {
-      rules: [
-        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-        { test: /\.tsx?$/, loader: "ts-loader" }
-      ]
+        rules: [
+            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: '/node_modules/'
+            },
+            {
+                test: /phaser\.js$/, 
+                loader: 'expose-loader?Phaser'
+            }
+        ]
     },
-    watch: false,
+    // watch: false,
     // watchOptions: {
     //     aggregateTimeout: 1000
-    // }
+    // },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'www'),
+    },
+
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: path.join(srcPath + 'index.html'),
+                to: path.resolve(__dirname, outDir)
+            },
+            {
+                from:  path.join(srcPath + 'assets'),
+                to: path.resolve(__dirname, outDir),
+            }
+        ])
+    ]
 };
